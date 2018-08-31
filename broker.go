aaa0e415626c922c062f1931327dd10ae34b9d6a
Sean Swehla <sean@sweh.la>
2018-08-30 21:20:59 -0400

    Fix error on client disconnect

--- broker.go
+++ broker.go
@@ -1,25 +1,42 @@
 package capans
 
-import "bytes"
+import (
+	"bytes"
+	"io"
+)
 
 var msgs = &MsgB0rker{}
 
 type MsgB0rker struct {
 	messages []*bytes.Buffer
+	free     []int
 }
 
 func (m *MsgB0rker) Stream() *Stream {
+	if len(m.free) == 0 {
+		id := len(m.messages)
+		s := &Stream{
+			id: id,
+			m:  m,
+		}
+		m.messages = append(m.messages, &bytes.Buffer{})
+		s.buf = m.messages[id]
+		return s
+	}
+	id := m.free[0]
+	m.free = m.free[1:]
 	s := &Stream{
-		id: len(m.messages),
+		id: id,
 		m:  m,
 	}
-	m.messages = append(m.messages, &bytes.Buffer{})
+	m.messages[id] = &bytes.Buffer{}
+	s.buf = m.messages[id]
 	return s
 }
 
 func (m *MsgB0rker) WriteFrom(id int, p []byte) (int, error) {
 	for i := range m.messages {
-		if i == id {
+		if i == id || m.messages[i] == nil {
 			continue
 		}
 		_, err := m.messages[i].Write(p)
@@ -30,23 +47,21 @@ func (m *MsgB0rker) WriteFrom(id int, p []byte) (int, error) {
 	return len(p), nil
 }
 
-func (m *MsgB0rker) ReadFor(id int, p []byte) (int, error) {
-	return m.messages[id].Read(p)
+func (m *MsgB0rker) Close(id int) {
+	m.messages[id] = nil
+	m.free = append(m.free, id)
 }
 
 type Stream struct {
-	id int
-	m  *MsgB0rker
-}
-
-func (s *Stream) Read(p []byte) (int, error) {
-	return s.m.ReadFor(s.id, p)
-}
-
-func (s *Stream) Write(p []byte) (int, error) {
-	return s.m.WriteFrom(s.id, p)
+	id  int
+	m   *MsgB0rker
+	buf io.Reader
 }
 
 func (s *Stream) Id() int {
 	return s.id
 }
+
+func (s *Stream) Read(p []byte) (int, error)  { return s.buf.Read(p) }
+func (s *Stream) Write(p []byte) (int, error) { return s.m.WriteFrom(s.id, p) }
+func (s *Stream) Close()                      { s.m.Close(s.id) }
